2b00c73a2ca65932296261e3f2f0f2398c353174
Sue Burt <sookburt@gmail.com>
2022-03-24 18:58:39 +0000

    Added more tests to compasDirection.test

--- test/compassDirection.test.ts
+++ test/compassDirection.test.ts
@@ -1,4 +1,4 @@
-import { isInvalidDirection } from '../src/compassDirection';
+import { isInvalidDirection, getNextLeft, getNextRight, getInitialDirection } from '../src/compassDirection';
 
 describe("InstructionSet", () => { 
 
@@ -9,11 +9,39 @@ describe("InstructionSet", () => {
     ["S", false],
     ["L", true],
     ["0", true],
-    ["*", true]
+    ["*", true],
+    ["", true],
+    ["SS", true]
   ])(`isInvalidDirection(%i) returns %i`,
 
     (x, y) => {
       expect(isInvalidDirection(x)).toBe(y);
+  });
+
+  it.each([
+    ["N", "W"],
+    ["W", "S"],
+    ["S", "E"],
+    ["E", "N"]
+  ])(`getNextLeft(%i) returns %i`,
+
+    (x, y) => {
+      expect(getNextLeft(x)).toBe(y);
+  });
+
+  it.each([
+    ["N", "E"],
+    ["W", "N"],
+    ["S", "W"],
+    ["E", "S"]
+  ])(`getNextRight(%i) returns %i`,
+
+    (x, y) => {
+      expect(getNextRight(x)).toBe(y);
+  });
+
+  test("getInitialDirection() always returns N", () => {
+    expect(getInitialDirection()).toBe("N");
   });
 
 });
