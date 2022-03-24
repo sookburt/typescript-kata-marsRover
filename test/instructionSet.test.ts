a237e6aa4865176448666852c15183639a576387
Sue Burt <sookburt@gmail.com>
2022-03-24 18:06:26 +0000

    Added new test suite for instructionSet

---
+++ test/instructionSet.test.ts
@@ -0,0 +1,19 @@
+import { InstructionSet } from "../src/instructionSet";
+import { Plateau } from "../src/plateau";
+import { closeInputStream } from '../src/console';
+
+afterEach(() => {
+  closeInputStream();
+});
+
+describe("InstructionSet", () => {
+
+  test("that an instruction set is created successfully", () => {
+
+    const instructionSet = new InstructionSet(0,0,"N","LRM",new Plateau(5,5)); 
+    expect(instructionSet.getInstructions()).toBe("x:0, y:0, direction:N, instruction:LRM, plateau:x:5, y:5");
+  });
+
+  
+
+});
